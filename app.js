8f9f67f77f3efbe8656e62d5e8a8663d47704aa6
Bobo <wambuinjumbi@gmail.com>
2020-07-06 14:07:20 +0300

    set up multer on app.js

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
 // import modules
 const express = require('express');
 const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const session = require('express-session');
+const ejs = require('ejs')
+const session = require('express-session')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const path = require('path');
 const multer = require('multer');
 
-//import routes
 const sequelize = require('./utilities/database')
-
-
-// app 
+// create server
 const app = express()
 
-//set up and configuration
+//set up
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + "-" + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false)
+    }
+};
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use(multer({
+    storage: fileStorage,
+    fileFilter: fileFilter
+}).single('image'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(session({
     secret: 'secret-key',
     store: new SequelizeStore({
@@ -30,12 +48,13 @@ app.use(session({
     proxy: true // if you do SSL outside of node.
 }));
 
-//listen to port
-sequelize.sync({
-        force: false
-    })
+//use routes
+
+
+//listen to server
+sequelize.sync()
     .then(() => {
-        app.listen(6000, () => {
+        app.listen(3000, () => {
             console.log('app started')
 
         });
